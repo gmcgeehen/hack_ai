f5745ad15bf4c56fa743208fca79f492f94aa6a6
gmcgeehen <102680262+gmcgeehen@users.noreply.github.com>
2025-02-22 11:22:38 -0500

    Co-authored-by: PeterErlenbusch <PeterErlenbusch@users.noreply.github.com> Initial image capture

--- RepCounter/rep_counter.py
+++ RepCounter/rep_counter.py
@@ -2,3 +2,43 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+
+
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(frame)
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+
+
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+
+    cv2.imshow("Pose Detection", frame)
+    if cv2.waitKey(10) & 0xff == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+    
